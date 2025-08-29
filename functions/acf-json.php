e05c8969fa03abc7849dd0ff59071578007a1bf6
Neal Chester <ncchester@gmail.com>
2025-08-29 05:53:47 -0400

    update

--- functions/acf-json.php
+++ functions/acf-json.php
@@ -4,7 +4,7 @@ add_filter('acf/settings/load_json', 'nc_acf_json_load_point');
 function nc_acf_json_load_point($paths)
 {
     unset($paths[0]);
-    $paths[] = get_template_directory() . '/acf-json';
+    $paths[] = get_template_directory() . '/acf';
     return $paths;
 }
 
